42db731b78b653eed6ff21b48e02984556133f95
JPasion <66902295+JheNaeumi@users.noreply.github.com>
2024-09-23 21:03:49 +0800

    Time: 512 ms (53.06%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 /* Write your T-SQL query statement below */
-select a.project_id, round(cast(avg(b.experience_years)as float),2) as average_years
+select a.project_id, round(avg(b.experience_years),2) as average_years
 from project a
 left join employee b
 on a.employee_id = b.employee_id
