9833f60eea663f516bd2f5374141a7b61583b60c
JPasion <66902295+JheNaeumi@users.noreply.github.com>
2024-10-13 21:21:07 +0800

    Time: 1539 ms (48.08%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,7 +1,10 @@
 /* Write your T-SQL query statement below */
-
+with product_count as (
+    select count(product_key) as allcount
+    from product
+) 
 select c.customer_id
 from customer c
  group by c.customer_id
- having count(distinct c.product_key) =  (select count(product_key) from product)
+ having count(distinct c.product_key) =  (select allcount from product_count)
  order by c.customer_id
