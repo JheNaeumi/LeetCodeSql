8a7e1add7b548e960a58e321443956e1212f4e7b
JPasion <66902295+JheNaeumi@users.noreply.github.com>
2024-12-14 15:20:55 +0800

    Time: 1075 ms (5.07%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,10 @@
+-- Solution to swap seats for consecutive students
+select 
+    case 
+        when id % 2 = 1 and id < (select max(id) from seat) then id + 1
+        when id % 2 = 0 then id - 1
+        else id
+    end as id,
+    student
+from seat
+order by id
